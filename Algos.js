25ccc9a219d8868e15f165118a01a10ce667fea5
Christopher Melby <melby.christopher@gmail.com>
2020-07-07 14:22:36 -0700

    aadding linked list practice

--- Algos.js
+++ Algos.js
@@ -433,3 +433,66 @@ for(let i = 0; i < arr.length; i++) {
 return pairs;
 }
 console.log(pairCount(array3))
+
+
+//=============== Rotate Array in Place ====================//
+
+/*Given an array, rotate the array to the right by k steps, where k is non-negative.
+Follow up:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  if(nums.length > k) {
+      nums.unshift(...nums.splice(-k))
+  } else {
+      let i = 0;
+      while(i < k) {
+          nums.unshift(nums.splice(-1))
+          i++
+      }
+  }
+    return nums
+    
+};
+
+//======================== REVERSE LINKED LIST =================================//
+var reverseList = function(head) {
+  let reversed = null;
+  while (head) {
+      // store initial order in temp variable
+      const next = head.next;
+      // assign the next node to what has been reversed so far
+      head.next = reversed;
+      // one more node has been reversed, assign reversed to the linked list
+      reversed = head;
+      // reset head to the original order minus one node that has been added to the reversed list
+      head = next;
+  }
+  return reversed;
+
+  // Recursive solution    
+  var recursiveReverse = (head, reversed) => {
+    if (!head) {
+        return reversed
+    }
+    const next = head.next;
+    head.next = reversed;
+    reversed = head;
+    return recursiveReverse(next, reversed)
+  }
+  
+  return recursiveReverse(head, null)
+};
--- linkedList.js
+++ linkedList.js
@@ -1,50 +1,128 @@
-let myLinkedList = {
-    head: {
-        value: 10,
-        next: {
-            value: 5, 
-            next: {
-                value: 16,
-                next: null
-            }
-        }
-    }
-}
-
-class LinkedList {
+// let myLinkedList = {
+//     head: {
+//         value: 10,
+//         next: {
+//             value: 5,
+//             next: {
+//                 value: 16,
+//                 next: null
+//             }
+//         }
+//     }
+// }
+// class Node { // create class to dry up code.
+//     constructor(value){
+//         this.value = value;
+//         this.next = null;
+//     }
+// }
+class linkedList {
     constructor(value) {
-
+        this.head = {
+            value: value,
+            next: null
+        }
+        this.tail = this.head;
+        this.length = 1;
+    }
+    append(value) {
+        // const newNode = new Node(value); To dry up code create new node from the class above instead creating a new node eveery time.
+        const newNode = { 
+            value: value,
+            next: null
+        };
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this
+    }
+    prepend(value) {
+        const newNode = {
+            value: value,
+            next: null
+        };
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length ++;
+        return this;
+    }
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+    insert(index, value) {
+        // check params
+        if(index >= this.length) {
+            return this.append(value);
+        }
+        const newNode = {
+            value: value,
+            next: null
+        };
+        const leader = this.traverseToIndex(index-1);
+        const holdingPointer = leader.next;
+        leader.next = newNode;
+    }
+    traverseToIndex(index) {
+        // check params
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index) {
+            currentNode = currentNode.next;
+            counter ++;
+        }
+        return currentNode;
+    } 
+    remove(index) {
+        // check params
+        const leader = this.traverseToIndex(index-1);
+        const unWantedNode = leader.next;
+        leader.next = unWantedNode.next;
+        this.length--;
+        return this.printList();
     }
 }
 
-const 
+const myLinkedList = new linkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.printList();
+myLinkedList.insert(2, 99);
+console.log(myLinkedList);
 
 
-var reverseList = function(head) {
-    let reversed = null;
-    while (head) {
-        // store initial order in temp variable
-        const next = head.next;
-        // assign the next node to what has been reversed so far
-        head.next = reversed;
-        // one more node has been reversed, assign reversed to the linked list
-        reversed = head;
-        // reset head to the original order minus one node that has been added to the reversed list
-        head = next;
-    }
-    return reversed;
+//======================== REVERSE LINKED LIST =================================//
+// var reverseList = function(head) {
+//     let reversed = null;
+//     while (head) {
+//         // store initial order in temp variable
+//         const next = head.next;
+//         // assign the next node to what has been reversed so far
+//         head.next = reversed;
+//         // one more node has been reversed, assign reversed to the linked list
+//         reversed = head;
+//         // reset head to the original order minus one node that has been added to the reversed list
+//         head = next;
+//     }
+//     return reversed;
 
-    // Recursive solution    
-    var recursiveReverse = (head, reversed) => {
-      if (!head) {
-          return reversed
-      }
-      const next = head.next;
-      head.next = reversed;
-      reversed = head;
-      return recursiveReverse(next, reversed)
-    }
+//     // Recursive solution    
+//     var recursiveReverse = (head, reversed) => {
+//       if (!head) {
+//           return reversed
+//       }
+//       const next = head.next;
+//       head.next = reversed;
+//       reversed = head;
+//       return recursiveReverse(next, reversed)
+//     }
     
-    return recursiveReverse(head, null)
-};
+//     return recursiveReverse(head, null)
+// };
 
