ca23c343370bd0a96bb29fa7445189332b834120
Christopher Melby <melby.christopher@gmail.com>
2020-08-29 07:38:18 -0700

    adding algo practice

--- Algos.js
+++ Algos.js
@@ -223,17 +223,7 @@ function palindromeCheck(str) {
 
 console.log(palindromeCheck('level'))
 
-function palindromeCheck(str) {
-  let reverseStr = str.split('').reverse().join('')
-
-  if(reverseStr === str) {
-    return true
-  } else {
-    return false
-  }
-}
-
-
+// Repeat this. 
 
 // RETURNS THE FACTORIAL OF NUM...................................................
 // Write code to create a function that returns the factorial of `num`
@@ -299,18 +289,6 @@ for (let i = 0; i < nums.length; i++) {
 }
 }
 console.log(twoSum(nums, target))
-
-let TwoSum = function(nums, target) {
-  let map = new Map;
-
-  for (let i = 0; i < nums.length; i++) {
-    let compliment = target - nums[i]
-    if(map.has(compliment)) {
-      return [map.get(compliment), i];
-    }
-    map.set(nums[i], i);
-  }
-}
 
 
 // FIRST REOCURRING CHARACTER.....................................................
