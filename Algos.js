bedfaed89bbd2415ba9b32158dcc2c5f8709d00d
Christopher Melby <melby.christopher@gmail.com>
2020-09-21 22:45:59 -0700

    adding algo practice

--- Algos.js
+++ Algos.js
@@ -592,3 +592,23 @@ var reverseList = function(head) {
 }
 
 
+let reverseStr = function(str) {
+  let reversedStr = str.split('').reverse().join('')
+  return reversedStr;
+}
+console.log(reversedStr)
+
+
+var nums = [2, 7, 8, 9];
+var target = 9
+let twoSum = function(num, target) {
+  let map = new Map;
+  for(let i = 0; i < nums.length; i++) {
+    let compliment = target - nums[i];
+    if(map.has(compliment)) {
+      return [map.get(compliment, i)]
+    }
+    map.set(nums[i], i)
+  }
+}
+console.log(twoSum())
